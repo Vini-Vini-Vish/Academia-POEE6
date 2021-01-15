10f746c17b019c7e68b973eac5fefd6e618daab3
Vinícius Henrique do Nascimento <68130981+Vini-Vini-Vish@users.noreply.github.com>
2021-01-15 15:47:01 -0300

    Create AtividadeService.java

---
+++ src/main/java/com/academia/model/service/AtividadeService.java
@@ -0,0 +1,125 @@
+package com.academia.model.service;
+
+import java.util.List;
+import javax.persistence.EntityTransaction;
+import com.academia.estrutura.util.VariaveisProjeto;
+import com.academia.model.dao.atividade.AtividadeDao;
+import com.academia.model.models.Atividade;
+
+public class AtividadeService extends ConexaoBancoService{
+	
+	private AtividadeDao atividadeDao;
+
+	public AtividadeService() {
+		this.atividadeDao = new AtividadeDao(this.getEntityManager());
+	}
+	
+	//-------------------------------------------------------------------
+	
+		public Integer save(Atividade atividade) {
+			
+			Integer toReturn = 0;
+			
+			EntityTransaction trx = this.getTransaction();
+			
+			toReturn = validarDigitacao(atividade);
+			
+			if(toReturn == VariaveisProjeto.DIGITACAO_OK) {
+			
+				try {
+					trx.begin();
+					this.getAtividadeDao().save(atividade);
+					trx.commit();
+					
+				}catch(Exception ex) {
+					ex.printStackTrace();
+					if(trx.isActive()) {
+						trx.rollback();
+					}
+					toReturn = VariaveisProjeto.ERRO_INCLUSAO;
+				}finally {
+					this.close();
+				}
+			}
+			
+			return toReturn;
+		}
+		
+		//-------------------------------------------------------------------
+		
+		public Integer update(Atividade atividade) {
+			
+			Integer toReturn =0;
+			
+			EntityTransaction trx = this.getTransaction();
+			
+			if(validarDigitacao(atividade)== VariaveisProjeto.DIGITACAO_OK) {
+			
+				try {
+					trx.begin();
+					this.getAtividadeDao().update(atividade);
+					trx.commit();
+					
+				}catch(Exception ex) {
+					ex.printStackTrace();
+					if(trx.isActive()) {
+						trx.rollback();
+					}
+					toReturn = VariaveisProjeto.ERRO_ALTERACAO;
+				}finally {
+					this.close();
+				}
+			}
+			
+			return toReturn;
+		}
+		
+		//-------------------------------------------------------------------
+		
+		public Integer delete(Atividade atividade) {
+			
+			Integer toReturn =0;
+			
+			EntityTransaction trx = this.getTransaction();			
+			
+				try {
+					trx.begin();
+					Atividade altiviadadeEncontrada = this.getAtividadeDao().findById(atividade.getIdAtividade());
+					this.getAtividadeDao().remove(altiviadadeEncontrada);
+					trx.commit();
+					
+				}catch(Exception ex) {
+					ex.printStackTrace();
+					if(trx.isActive()) {
+						trx.rollback();
+					}
+					toReturn = VariaveisProjeto.ERRO_ALTERACAO;
+				}finally {
+					this.close();
+				}
+			
+			return toReturn;
+		}
+		
+		//-------------------------------------------------------------------
+		
+		public Atividade findById(Integer idAtividade) {
+			return this.getAtividadeDao().findById(idAtividade);
+		}
+		
+		public List<Atividade> findAll(){
+			return this.getAtividadeDao().findAll(Atividade.class);
+		}
+		
+		public Integer validarDigitacao(Atividade atividade) {
+			if(VariaveisProjeto.digitacaoCampo(atividade.getNome())) {
+				return VariaveisProjeto.NOME_CAMPO_VAZIO;
+			}		
+			return VariaveisProjeto.DIGITACAO_OK;
+		}	
+
+		public AtividadeDao getAtividadeDao() {
+			return atividadeDao;
+		}
+	
+}
