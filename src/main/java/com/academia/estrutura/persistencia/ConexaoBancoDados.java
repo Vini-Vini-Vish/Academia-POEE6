cd5e5cf038a8a7f115fd4687a679c1aae60c5b98
Vinícius Henrique do Nascimento <68130981+Vini-Vini-Vish@users.noreply.github.com>
2021-01-14 23:48:50 -0300

    Update ConexaoBancoDados.java

--- src/main/java/com/academia/estrutura/persistencia/ConexaoBancoDados.java
+++ src/main/java/com/academia/estrutura/persistencia/ConexaoBancoDados.java
@@ -37,7 +37,7 @@ public class ConexaoBancoDados {
 		
 		Map<String, String> properties = new HashMap<String, String>();
 		
-		properties.put("javax.persistence.schema-generation.database.action", "drop-and-create");
+		properties.put("javax.persistence.schema-generation.database.action", "update");
 		properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
 		properties.put("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
 		properties.put("hibernate.connection.url", "jdbc:mysql://localhost:3306/projeto?createDatabaseIfNotExist=true&useSSL=false");
