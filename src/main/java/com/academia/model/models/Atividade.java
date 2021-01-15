790f25ea9cb505a6a3fb52d457df4f5c577f8b22
Vinícius Henrique do Nascimento <68130981+Vini-Vini-Vish@users.noreply.github.com>
2021-01-15 15:46:35 -0300

    Create Atividade.java

---
+++ src/main/java/com/academia/model/models/Atividade.java
@@ -0,0 +1,49 @@
+package com.academia.model.models;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name = "TAB_ATIVIDADES")
+public class Atividade {
+	
+	private Integer IdAtividade;
+	private String nome;
+	private String descricao;
+	
+	//-----------------------------------------------------------------//
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ATIVIDADE_ID")
+	public Integer getIdAtividade() {
+		return IdAtividade;
+	}
+	public void setIdAtividade(Integer idAtividade) {
+		IdAtividade = idAtividade;
+	}
+	
+	//-----------------------------------------------------------------//
+	
+	@Column(name = "ATIVIDADE_NAME", length = 100, nullable = false)
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	//-----------------------------------------------------------------//
+	
+	@Column(name = "ATIVIDADE_DESCRICAO", length = 100, nullable = false)
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	//-----------------------------------------------------------------//
+}
