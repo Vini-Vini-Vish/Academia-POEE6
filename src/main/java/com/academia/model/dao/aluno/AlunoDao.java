1250df58a8f688c50ffc1ed3966ddb7aa3a1c15e
Vinícius Henrique do Nascimento <68130981+Vini-Vini-Vish@users.noreply.github.com>
2021-01-17 18:43:16 -0300

    Update AlunoDao.java

--- src/main/java/com/academia/model/dao/aluno/AlunoDao.java
+++ src/main/java/com/academia/model/dao/aluno/AlunoDao.java
@@ -1,12 +1,32 @@
 package com.academia.model.dao.aluno;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import com.academia.model.models.Aluno;
 
 public class AlunoDao extends AlunoGenericDao <Aluno, Integer> {
 	
 	public AlunoDao(EntityManager entityManager) {
 		super(entityManager);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Aluno> listAlunoPaginacao(int numeroPagina, Integer defaultPagina) {
+		
+		List<Aluno> listaAluno = new ArrayList<Aluno>();
+		
+		boolean ativo = true;
+		
+		
+		Query query = this.getEntityManager().createQuery("SELECT u FROM Usuario u WHERE u.ativo =:ativo")
+											 .setParameter("ativo", ativo)
+											 .setFirstResult(numeroPagina)
+											 .setMaxResults(defaultPagina);
+		listaAluno = query.getResultList();
+		
+		return listaAluno;
 	}
 
 }
