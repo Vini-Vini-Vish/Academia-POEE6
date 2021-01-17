51aa69bb962fcd7bf34f6bf1fc12fd3200410a31
Vinícius Henrique do Nascimento <68130981+Vini-Vini-Vish@users.noreply.github.com>
2021-01-17 18:43:22 -0300

    Update PersonalDao.java

--- src/main/java/com/academia/model/dao/personal/PersonalDao.java
+++ src/main/java/com/academia/model/dao/personal/PersonalDao.java
@@ -1,12 +1,31 @@
 package com.academia.model.dao.personal;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.EntityManager;
-
+import javax.persistence.Query;
 import com.academia.model.models.Personal;
 
 public class PersonalDao extends PersonalGenericDao<Personal, Integer>{
 	
 	public PersonalDao(EntityManager entityManager) {
 		super(entityManager);
-	}	
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Personal> listPersonalPaginacao(int numeroPagina, Integer defaultPagina) {
+		
+		List<Personal> listaPersonal = new ArrayList<Personal>();
+		
+		boolean ativo = true;
+		
+		
+		Query query = this.getEntityManager().createQuery("SELECT u FROM Usuario u WHERE u.ativo =:ativo")
+											 .setParameter("ativo", ativo)
+											 .setFirstResult(numeroPagina)
+											 .setMaxResults(defaultPagina);
+		listaPersonal = query.getResultList();
+		
+		return listaPersonal;
+	}
 }
