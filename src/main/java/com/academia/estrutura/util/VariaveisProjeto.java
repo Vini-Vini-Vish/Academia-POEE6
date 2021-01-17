4295949294dd69b08901d9af15dd97159a9ea3e7
Vinícius Henrique do Nascimento <68130981+Vini-Vini-Vish@users.noreply.github.com>
2021-01-16 22:12:46 -0300

    Update VariaveisProjeto.java

--- src/main/java/com/academia/estrutura/util/VariaveisProjeto.java
+++ src/main/java/com/academia/estrutura/util/VariaveisProjeto.java
@@ -5,19 +5,56 @@ import java.util.Objects;
 public class VariaveisProjeto {
 	
 	public static final String PERSISTENCE_UNIT_NAME = "projeto";
+	
+	//-------------------------------------------------------------------
 		
 	public static final Integer ERRO_INCLUSAO = 10;
 	public static final Integer ERRO_ALTERACAO = 20;
 	public static final Integer ERRO_EXCLUSAO = 30;
 	
+	//-------------------------------------------------------------------
+	
 	public static final Integer INCLUSAO_REALIZADA = 1;
 	public static final Integer	ALTERACAO_REALIZADA = 2;
 	public static final Integer EXCLUSAO_REALIZADA = 3;
 	
-	public static final Integer DIGITACAO_OK = 100;
-	public static final Integer NOME_CAMPO_VAZIO = 200;
+	//-------------------------------------------------------------------
 	
+	public static final Integer DIGITACAO_OK = 100;
+	
+	//-------------------------------------------------------------------
+	
+	//Usuario
+	public static final Integer USUARIO_USER_NAME = 201;
+	public static final Integer USUARIO_EMAIL = 202;
+	public static final Integer USUARIO_PASSWORD = 203;
+	
+	//-------------------------------------------------------------------
+	
+	//Departamento
+	public static final Integer DEPARTAMENTO_NOME = 300;
+	
+	//-------------------------------------------------------------------
+	
+	//Aluno //Personal //Atividade
+	public static final Integer DESCRICAO = 400;
+	public static final Integer NOME = 401;
+	public static final Integer GENERO = 402;
+	public static final Integer IDADE = 403;
+	public static final Integer ENDERECO = 404;
+	public static final Integer NUMERO = 405;
+	public static final Integer BAIRRO = 406;
+	public static final Integer CIDADE = 407;
+	public static final Integer TELEFONE = 408;
+	public static final Integer CODIGO_POSTAL = 409;
+	public static final Integer PERIODO = 410;
+	public static final Integer CREF = 411;
+	
+	//-------------------------------------------------------------------
+		
 	public static final String LIMPA_CAMPO = "";
+	
+	//-------------------------------------------------------------------
 	
 	public static boolean digitacaoCampo(Integer texto) {
 		if(Objects.isNull(texto)) {
@@ -31,6 +68,8 @@ public class VariaveisProjeto {
 		return false;
 	}
 	
+	//-------------------------------------------------------------------
+	
 	public static boolean digitacaoCampo(String texto) {
 		if(Objects.isNull(texto)) {
 			return true;
@@ -43,6 +82,8 @@ public class VariaveisProjeto {
 		return false;
 	}
 
+	//-------------------------------------------------------------------
+	
 	public static Integer convertToInteger(String text) {
 		return Integer.parseInt(text);
 	}
