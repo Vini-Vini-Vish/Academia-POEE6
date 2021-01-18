977ec08c0c32aafb435b9be1f9c33527ab1166bf
Vinícius Henrique do Nascimento <68130981+Vini-Vini-Vish@users.noreply.github.com>
2021-01-18 09:46:13 -0300

    Update AtividadeDao.java

--- src/main/java/com/academia/model/dao/atividade/AtividadeDao.java
+++ src/main/java/com/academia/model/dao/atividade/AtividadeDao.java
@@ -1,12 +1,31 @@
 package com.academia.model.dao.atividade;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import com.academia.model.models.Atividade;
 
 public class AtividadeDao extends AtividadeGenericDao <Atividade, Integer> {
 
 	public AtividadeDao(EntityManager entityManager) {
 		super(entityManager);		
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Atividade> listAtividadePaginacao(int numeroPagina, Integer defaultPagina) {
+		List<Atividade> listaAtividade = new ArrayList<Atividade>();
+		
+		boolean ativo = true;
+		
+		
+		Query query = this.getEntityManager().createQuery("SELECT u FROM Usuario u WHERE u.ativo =:ativo")
+											 .setParameter("ativo", ativo)
+											 .setFirstResult(numeroPagina)
+											 .setMaxResults(defaultPagina);
+		listaAtividade = query.getResultList();
+		
+		return listaAtividade;
 	}
 
 }
